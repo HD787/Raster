bf1f5a17e7c90c56133bbcf054a455eed797f7a9
henry <Hd116527@gmail.com>
2024-04-17 09:40:46 -0500

    removed unused types

--- types.h
+++ types.h
@@ -1,12 +1,12 @@
 typedef unsigned char byte;
 
-typedef struct{
-    float x, y, z, w;
-}vec4;
+// typedef struct{
+//     float x, y, z, w;
+// }vec4;
 
-typedef struct{
-    vec4 one, two, three;
-}triangle;
+// typedef struct{
+//     vec4 one, two, three;
+// }triangle;
 
 typedef struct{
     int* vertices;
